405f9fd75c2bfee8477b0812479e18ced00129bf
Keito Fukuoka <keito.0o0.kirby@gmail.com>
2024-11-02 15:53:03 +0000

    feat: remove hyde.py and add paths.py
    - Removed `hyde.py` file from the project.
    - Added `paths.py` to `aeug/utils` directory.
    - Updated `requirements3.txt` to include `autoawq` dependency.

---
+++ aeug/utils/paths.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+# CACHE_DIR = Path.home() / ".cache" / "aeug"
+CACHE_DIR = Path(__file__).parents[2]
+VALOUTPUT_DIR = CACHE_DIR / "val_output"
+
+QUANT_DIR = CACHE_DIR / "quant"
