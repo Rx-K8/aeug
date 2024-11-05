85c043d3e414ba5c13c8240e2726f2cbc4c03172
Keito Fukuoka <keito.0o0.kirby@gmail.com>
2024-11-05 13:53:09 +0000

    feat: integrate Contriever for similarity scoring
    - Added import statements for AutoModel, AutoTokenizer, and Contriever.
    - Updated TEMPLATE to output questions as a numbered list.
    - Integrated Contriever for similarity scoring in the Aeug class.
    - Added a similarity method to calculate dot score between query and content.
    - Updated search method to generate questions and calculate similarity scores.
    - Added exception handling for question generation.
    - Updated main section to use a different model configuration.

--- aeug/search/aeug_search.py
+++ aeug/search/aeug_search.py
@@ -1,6 +1,11 @@
 import textwrap
 
+from transformers import AutoModel, AutoTokenizer
+
+from aeug.contriever import Contriever
 from aeug.generation.generator import Generator
+from aeug.logger_config import logger
+from aeug.outputs.questios import Questions
 from aeug.prompt.prompter import Prompter
 from aeug.search.bm25 import BM25
 from aeug.utils.typing import CommonSearchOutput
@@ -8,13 +13,13 @@ from aeug.utils.typing import CommonSearchOutput
 TEMPLATE = textwrap.dedent(
     """\
     Please create five questions that can be considered from the document.
+    Please output as a numbered list.
     Only respond with the queries, nothing else.
-    Your response should be a list of comma separated values, eg: `foo, bar, baz` or `foo,bar,baz`
 
     ### Document:
     {}
 
-    ### five queries:
+    ### Five questions:
     """
 )
 
@@ -24,20 +29,39 @@ class Aeug:
         self.prompter = Prompter(TEMPLATE)
         self.first_step = BM25(benchmark_name)
         self.generator = Generator(model_id)
+        self.contriever = Contriever()
+
+    def similarity(self, query: str, content: str):
+        score = self.contriever.dot_score(query, content)
+        return score
 
     def search(self, query, top_k: int = 10):
         first_hits: list[CommonSearchOutput] = self.first_step.search(
             query, top_k
         )
         for hit in first_hits:
-            print(hit.content)
             prompt = self.prompter.generate(hit.content)
-            queries = self.generator.generate(prompt)
-            print(queries)
-            break
+            output = self.generator.generate(prompt)
+            try:
+                questions = Questions.from_string(output)
+            except Exception as e:
+                logger.exception(e)
+
+            for question in questions:
+                question_score = self.similarity(query, question)
+                print(question_score)
+
+
+
 
 
 if __name__ == "__main__":
     # aeug = Aeug("meta-llama/Llama-3.1-70B-Instruct", "trec2020")
-    aeug = Aeug("meta-llama/Llama-3.1-8B-Instruct", "trec2020")
+    # aeug = Aeug("meta-llama/Llama-3.1-8B-Instruct", "trec2020")
+    # aeug = Aeug(
+    #     "hugging-quants/Meta-Llama-3.1-8B-Instruct-AWQ-INT4", "trec2020"
+    # )
+    aeug = Aeug(
+        "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4", "trec2020"
+    )
     print(aeug.search("What is python proggraming language?"))
